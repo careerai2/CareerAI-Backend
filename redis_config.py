8c23077e938192f715a552bfc59e2b79abb30a06
Keshav-Raj-098 <keshavraj09898@gmail.com>
2025-08-01 01:52:19 +0530

    redis env added

--- redis_config.py
+++ redis_config.py
@@ -1,4 +1,11 @@
-
 import redis
+import os
 
-redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+redis_port = int(os.environ.get('REDIS_PORT', 6379))  # cast to int
+
+redis_client = redis.Redis(
+    host=redis_host,
+    port=redis_port,
+    decode_responses=True
+)
